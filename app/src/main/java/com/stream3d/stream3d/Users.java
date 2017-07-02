f1467eac659c8a2524f5a9a42ad227de4c700ad5
TimBluesWin <timisutantyo@gmail.com>
2017-07-02 23:36:07 +0800

    Adding helper functions

---
+++ app/src/main/java/com/stream3d/stream3d/Users.java
@@ -0,0 +1,32 @@
+package com.stream3d.stream3d;
+
+public class Users
+{
+    private String name, user, password;
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setUser(String user)
+    {
+        this.user = user;
+    }
+    public String getUser()
+    {
+        return user;
+    }
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+    public String getPassword()
+    {
+        return password;
+    }
+}
